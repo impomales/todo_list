51d18d198bea56f37501730a2973708a29fad886
Isaias <impomales@gmail.com>
2018-01-07 23:41:32 +0000

    define todo schema

--- todos_api/models/index.js
+++ todos_api/models/index.js
@@ -2,3 +2,5 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost/todo-api');
 mongoose.Promise = Promise;
+
+module.exports.Todo = require('./todo.js');
