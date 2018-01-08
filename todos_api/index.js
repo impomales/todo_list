cbefe01267f14a8384637ec2a45b1c3bc2f0e914
Isaias <impomales@gmail.com>
2018-01-08 22:20:45 +0000

    set up for serving static files

--- todos_api/index.js
+++ todos_api/index.js
@@ -8,8 +8,11 @@ var todoRoutes = require('./routes/todos.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
 app.get('/', function(req, res) {
-    res.send('home');
+    res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes);
