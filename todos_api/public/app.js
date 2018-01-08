ff046e0753c3e9d0a9b04921fd1e28cdcff593fa
Isaias <impomales@gmail.com>
2018-01-08 23:36:58 +0000

    can add new todo using api

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -5,12 +5,36 @@ $(document).ready(function() {
         .catch(function(err) {
             console.log(err);
         });
+        
+    // 13 is symbol for 'enter' key.
+    // createTodo function is called when enter key is pressed.
+    $('#todoInput').keypress(function(event) {
+       if (event.which == 13) {
+           createTodo();
+       } 
+    });
 });
 
 function addTodos(todos) {
     todos.forEach(function(todo) {
-        var newTodo = $('<li class="task">' + todo.name + '</li>');
-        if (todo.completed) newTodo.addClass('done');
-        $('.list').append(newTodo);
+        addTodo(todo);
     });
+}
+
+function addTodo(todo) {
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    if (todo.completed) newTodo.addClass('done');
+    $('.list').append(newTodo);
+}
+
+function createTodo() {
+    var usrInput = $('#todoInput').val();
+    $.post('/api/todos', { name: usrInput })
+        .then(function(newTodo) {
+            addTodo(newTodo);
+            $('#todoInput').val('');
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
