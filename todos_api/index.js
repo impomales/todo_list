21bde3b22dc64baba35c1d2f1d8fba2a56ae38e3
Isaias <impomales@gmail.com>
2018-01-08 05:12:14 +0000

    add bodyParser for parsing json requests

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,8 +1,12 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
     
 var todoRoutes = require('./routes/todos.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.send('home');
