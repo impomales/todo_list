9f4e82c74d6b4236e61f52025c9658763553d28f
Isaias <impomales@gmail.com>
2018-01-08 05:12:34 +0000

    add route for adding a new todo

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -3,13 +3,26 @@
      db = require('../models');
      
 router.get('/', function(req, res) {
-   db.Todo.find()
-    .then(function(todos) {
-        res.json(todos);
-    })
-    .catch(function(err) {
+    // status 200 OK
+    db.Todo.find()
+        .then(function(todos) {
+            res.json(todos);
+        })
+        .catch(function(err) {
         res.send(err);
-    });
+        });
+});
+
+router.post('/', function(req, res) {
+    // req.body := { name: todo name }
+    // status 201 created.
+    db.Todo.create(req.body)
+        .then(function(newTodo) {
+           res.status(201).json(newTodo);
+        })
+        .catch(function(err) {
+            res.send(err);
+        });
 });
 
 module.exports = router;
