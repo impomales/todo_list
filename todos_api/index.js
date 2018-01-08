15db29960392a81de78baeab7204db72bbbb7b78
Isaias <impomales@gmail.com>
2018-01-08 04:37:19 +0000

    add get route for all todos

--- todos_api/index.js
+++ todos_api/index.js
@@ -2,9 +2,13 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
     
+var todoRoutes = require('./routes/todos.js');
+
 app.get('/', function(req, res) {
     res.send('home');
 });
+
+app.use('/api/todos', todoRoutes);
     
 app.listen(port, function() {
     console.log('app is running on port: ' + port);
