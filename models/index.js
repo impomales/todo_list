dae3f4dab8cfbf98cc368c3c8c82c231c7cad50e
Isaias <impomales@gmail.com>
2018-01-09 00:37:07 +0000

    reorganize files for heroku deployment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api', { useMongoClient: true });
+mongoose.connect('mongodb://imp888:izzi1231@ds245277.mlab.com:45277/todos', { useMongoClient: true });
 mongoose.Promise = Promise;
 
 module.exports.Todo = require('./todo.js');
