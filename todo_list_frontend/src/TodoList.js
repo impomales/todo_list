b417a15e78a89e2da1c09b35708b184bd0d61c6d
Isaias <impomales@gmail.com>
2018-02-02 07:21:35 +0000

    error handling for loadTodos

--- todo_list_frontend/src/TodoList.js
+++ todo_list_frontend/src/TodoList.js
@@ -1,6 +1,39 @@
+/* global fetch */
 import React, { Component } from 'react';
 
+const APIURL = '/api/todos';
+
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+    }
+    
+    componentWillMount() {
+        this.loadTodos();
+    }
+    
+    loadTodos() {
+        fetch(APIURL)
+            .then(resp => {
+                if (!resp.ok) {
+                    if (resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = { errMessage: data.message };
+                            throw err;
+                        });
+                    } else {
+                        let err = { errorMessage: 'Please try again later, server is not responding.' };
+                        throw err;
+                    }
+                }
+                return resp.json();
+            })
+            .then(todos => this.setState({ todos }));
+    }
+    
     render() {
         return (<h1>Todo List!</h1>);
     }
