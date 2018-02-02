cdd56f0c05226e4bab2c3b18efa0122d740196ba
Isaias <impomales@gmail.com>
2018-02-02 07:55:32 +0000

    form to add a new todo

--- todo_list_frontend/src/TodoList.js
+++ todo_list_frontend/src/TodoList.js
@@ -1,7 +1,7 @@
 /* global fetch */
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
-
+import TodoForm from './TodoForm';
 const APIURL = '/api/todos';
 
 class TodoList extends Component {
@@ -10,6 +10,7 @@ class TodoList extends Component {
         this.state = {
             todos: []
         };
+        this.addTodo = this.addTodo.bind(this);
     }
     
     componentWillMount() {
@@ -35,6 +36,33 @@ class TodoList extends Component {
             .then(todos => this.setState({ todos }));
     }
     
+    addTodo(val) {
+        fetch(APIURL, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: val })
+        })
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = { errMessage: data.message };
+                        throw err;
+                    });
+                } else {
+                    let err = { errorMessage: 'Please try again later, server is not responding.' };
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(newTodo => {
+            this.setState({ todos: [...this.state.todos, newTodo] });
+        });
+    }
+    
     render() {
         const todos = this.state.todos.map((t) => (
             <TodoItem 
@@ -44,6 +72,7 @@ class TodoList extends Component {
         return (
             <div>
                 <h1>Todo List!</h1>
+                <TodoForm addTodo={ this.addTodo }/>
                 <ul>
                     { todos }
                 </ul>
