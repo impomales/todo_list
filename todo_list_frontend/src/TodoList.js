d4a8ca6d10ab3be274fed4e074ee6289ac59175e
Isaias <impomales@gmail.com>
2018-02-04 19:15:51 +0000

    can toggle whether todo is completed

--- todo_list_frontend/src/TodoList.js
+++ todo_list_frontend/src/TodoList.js
@@ -89,13 +89,50 @@ class TodoList extends Component {
         });
     }
     
+    toggleTodo(todo) {
+        const updateUrl = APIURL + '/' + todo._id;
+        
+        fetch(updateUrl, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ completed: !todo.completed })
+        })
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = { errMessage: data.message };
+                        throw err;
+                    });
+                } else {
+                    let err = { errorMessage: 'Please try again later, server is not responding.' };
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(updatedTodo => {
+            /* return toggled todo if equaled to updated todo,
+                otherwise just return todo */
+            const todos = this.state.todos.map(todo => 
+                (todo._id === updatedTodo._id) ?
+                    {...todo, completed: !todo.completed } :
+                    todo
+                );
+            this.setState({ todos: todos });
+        });
+    }
+    
     render() {
         const todos = this.state.todos.map((t) => (
             <TodoItem 
                 key={ t._id }
                 { ...t }
                 /* must bind to specific todo, not todolist */
-                onDelete={ this.deleteTodo.bind(this, t._id) }/>
+                onDelete={ this.deleteTodo.bind(this, t._id) }
+                onToggle={ this.toggleTodo.bind(this, t) }/>
         ));
         return (
             <div>
