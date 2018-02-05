cd8237d46b777066d08af641003df96d609b3610
Isaias <impomales@gmail.com>
2018-02-05 22:26:43 +0000

    use path module to get directories

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    path = require('path');
     
 var todoRoutes = require('./routes/todos.js');
 
@@ -11,10 +12,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(express.static(__dirname + '/public'));
 // app.use(express.static(__dirname + '/views'));
 
-app.use(express.static(__dirname + '/todo_list_frontend'));
+app.use(express.static(path.join(__dirname, 'todo_list_frontend','build')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/todo_list_frontend/build/index.html');
+    res.sendFile(path.join(__dirname, 'todo_list_frontend', 'build', 'index.html'));
 });
 
 app.use('/api/todos', todoRoutes);
